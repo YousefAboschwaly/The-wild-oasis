6970983ac06297245e36c9f7e98c7f5749eb23b4
yousef <yousefshwaly@gmail.com>
2025-10-09 18:14:16 +0300

    Share this file to through the project

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -3,3 +3,4 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = 'https://ersqmuthcoyjqdgtqytk.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
+export default supabase

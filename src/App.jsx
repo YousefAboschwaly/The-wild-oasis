d7adaae2c24dc401088a6acccded65ff7fa9d93c
yousef <yousefshwaly@gmail.com>
2025-10-10 18:23:32 +0300

    Create CabinTable and getCabins using RQ

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false}  />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>

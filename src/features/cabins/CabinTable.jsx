73494d2ef9cdb5eb2f623e3eb806c097386c2670
yousef <yousefshwaly@gmail.com>
2025-10-16 10:21:25 +0300

    make space asside image in cabins table

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -15,7 +15,7 @@ const Table = styled.div`
 `;
 
 const TableHeader = styled.header`
-  display: grid;
+ display: grid;
   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
   column-gap: 2.4rem;
   align-items: center;

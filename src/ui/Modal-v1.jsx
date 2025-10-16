722f72a62d422ff4742849cd9743e015e1044e24
yousef <yousefshwaly@gmail.com>
2025-10-16 22:33:45 +0300

    Create New Modal with efficient way called Compound component React-Pattern

--- src/ui/Modal-v1.jsx
+++ src/ui/Modal-v1.jsx
@@ -1,4 +1,3 @@
-import { createContext, useContext, useState } from "react";
 import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
@@ -52,25 +51,14 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext()
-function Modal({children}){
-  const [openName, setOpenName] = useState("")
-  const close = ()=> setOpenName("")
-  const open = setOpenName
-  return children
-}
-function Open({children , opens}){
-  const {open} = useContext(ModalContext)
-  return children
-}
-
- function Window({ children, onClose }) {
+function Modal({ children, onClose }) {
   return createPortal(
     <Overlay>
       <StyledModal>
         <Button onClick={onClose}>
           <HiXMark />
         </Button>
+
         <div>{children}</div>
       </StyledModal>
     </Overlay>,
@@ -78,5 +66,4 @@ function Open({children , opens}){
   );
 }
 
-Modal.Open = Open
-Modal.Window = Window
+export default Modal;

b86fe10137b5939109754fee59efdbf3470f8ff0
yousef <yousefshwaly@gmail.com>
2025-10-18 19:07:37 +0300

    add Key for each FilterButton and add disabled when i in the button

--- src/ui/Filter.jsx
+++ src/ui/Filter.jsx
@@ -35,20 +35,27 @@ const FilterButton = styled.button`
   }
 `;
 
-export default function Filter({filterField, options}) {
+export default function Filter({ filterField, options }) {
   const [searchParams, setSearchParams] = useSearchParams();
-  const currentFilter = searchParams.get(filterField)
+  const currentFilter = searchParams.get(filterField);
 
   function handleClick(value) {
-    searchParams.set(filterField,value);
+    searchParams.set(filterField, value);
     setSearchParams(searchParams);
   }
 
   return (
     <StyledFilter>
-      {options?.map(option=>  <FilterButton onClick={() => handleClick(option.value)} active={option.value === currentFilter}>{option.label}</FilterButton>)}
-    
-     
+      {options?.map((option) => (
+        <FilterButton
+        key={option.value}
+          onClick={() => handleClick(option.value)}
+          active={option.value === currentFilter}
+          disabled={option.value === currentFilter}
+        >
+          {option.label}
+        </FilterButton>
+      ))}
     </StyledFilter>
   );
 }

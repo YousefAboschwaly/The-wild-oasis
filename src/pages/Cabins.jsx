d23b7c6bcef97f68528b6cf7fe010796a47d9644
yousef <yousefshwaly@gmail.com>
2025-10-18 15:19:56 +0300

    Add Cabin TableOperations to make Filter and use it in Cabins Page

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,4 +1,5 @@
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTableOperations from "../features/cabins/CabinTableOperations";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "./../features/cabins/CabinTable";
@@ -9,7 +10,7 @@ function Cabins() {
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTableOperations/>
       </Row>
       <Row>
         <CabinTable />

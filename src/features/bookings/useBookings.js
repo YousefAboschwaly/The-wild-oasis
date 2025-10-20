68481b3e560fe2a9989cbc54dd0b9f0fa7a2a040
yousef <yousefshwaly@gmail.com>
2025-10-20 12:39:16 +0300

    handle useBookings to get Filtered vallue

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,14 +1,19 @@
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+  const [searchParams] = useSearchParams();
+  const filter = searchParams.get("status") || "all";
+
   const {
     isPending,
     error,
     data: bookings,
   } = useQuery({
-    queryKey: ["bookings"],
-    queryFn: getBookings,
+    queryKey: ["bookings", filter],
+    queryFn: () => getBookings(filter),
   });
+
   return { isPending, error, bookings };
 }
--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,12 +1,17 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings() {
-  let { data, error } = await supabase
+export async function getBookings(status) {
+  console.log(status)
+  let query=  supabase
     .from("bookings")
     .select(
       "id,created_at,startDate,endDate,numNights,numGuests,status,totalPrice, cabins(name),guests(fullName,email)"
-    );
+    )
+    if(status !== "all"){
+      query = query.eq("status",`${status}`);
+    }
+    let  { data, error } = await query
   if (error) {
     console.error(error);
     throw new Error("Bookings could not be loaded");

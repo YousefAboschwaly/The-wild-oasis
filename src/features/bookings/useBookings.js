35c6b5f1a7ca1aa34e7b17e5bd5718507e441007
yousef <yousefshwaly@gmail.com>
2025-10-21 15:21:29 +0300

    Make prefetchQuery to fetch the next page

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,9 +1,11 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
 import SortBy from "./../../ui/SortBy";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
+  const queryClient = useQueryClient()
   const [searchParams] = useSearchParams();
 
   // FILTER
@@ -26,6 +28,15 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy,page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+
+  // PREFETCHING
+  const pageCount = Math.ceil(count/PAGE_SIZE)
+  if(page <pageCount)
+  queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+    queryFn: () => getBookings({ filter, sortBy,page: page + 1 }),
+  })
 
   return { isPending, error, bookings, count };
 }

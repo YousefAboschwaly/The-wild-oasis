744a9171da6234786b7798abf129d1c90e9d6586
yousef <yousefshwaly@gmail.com>
2025-10-22 11:23:34 +0300

    Create delete booking hook

---
+++ src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,16 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
+import toast from "react-hot-toast";
+
+export function useDeleteBooking() {
+  const queryClient = useQueryClient()
+  const {mutate:deleteBooking  , isPending:isDeleting} = useMutation({
+    mutationFn: (bookingId) => deleteBookingApi(bookingId),
+    onSuccess:()=>{
+      toast.success("Booking successfully deleted")
+      queryClient.invalidateQueries({active:true})
+    },
+    onError:(err)=>toast.error(err.message)
+  });
+  return{deleteBooking , isDeleting}
+}

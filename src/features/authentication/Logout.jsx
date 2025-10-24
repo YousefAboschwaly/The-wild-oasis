314e794e3edd346ab4d172b2f3626607ef6d537b
yousef <yousefshwaly@gmail.com>
2025-10-24 23:42:12 +0300

    using useLogout hook in logout component

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ButtonIcon from './../../ui/ButtonIcon';
-import { HiArrowRightOnRectangle } from 'react-icons/hi2';
+import React from "react";
+import ButtonIcon from "./../../ui/ButtonIcon";
+import { HiArrowRightOnRectangle } from "react-icons/hi2";
+import { useLogout } from "./useLogout";
+import SpinnerMini from "../../ui/SpinnerMini";
 
 export default function Logout() {
+  const { logout, isLoginOut } = useLogout();
   return (
-    <ButtonIcon>
-      <HiArrowRightOnRectangle/>
+    <ButtonIcon disabled={isLoginOut} onClick={logout}>
+      {!isLoginOut ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
     </ButtonIcon>
-  )
+  );
 }

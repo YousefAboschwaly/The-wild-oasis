9dd76ae3e2d2fada3d9f54a3ddc46ccb66d38a29
yousef <yousefshwaly@gmail.com>
2025-10-24 23:14:31 +0300

    when logout is success to navigate to login page

--- src/features/authentication/useLogout.js
+++ src/features/authentication/useLogout.js
@@ -1,9 +1,14 @@
 import { useMutation } from "@tanstack/react-query";
 import { logout as logoutApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
 
 export  function useLogout() {
+  const navigate = useNavigate()
   const {mutate:logout , isPending:isLoginOut} = useMutation({
-    mutationFn:logoutApi
+    mutationFn:logoutApi,
+    onSuccess:()=>{
+      navigate('/login',{replace:true})
+    }
   })
 
   return {logout , isLoginOut}

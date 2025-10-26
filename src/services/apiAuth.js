c8eaa428ad39c592f420a14a9501720693035f1a
yousef <yousefshwaly@gmail.com>
2025-10-26 17:56:06 +0300

    Craete updateCurrent user function to update data of user

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,6 +1,7 @@
 import supabase from "./supabase";
+const SUPBASEURL = import.meta.env.VITE_SUPABASE_URL;
 
-export async function signup({ email, password, fullName }) { 
+export async function signup({ email, password, fullName }) {
   const { data, error } = await supabase.auth.signUp({
     email,
     password,
@@ -36,4 +37,35 @@ export async function getCurrentUser() {
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ fullName, password, avatar }) {
+  let updateData;
+
+  // 1 handle data that will be updated
+  if (fullName) updateData = { data: { fullName } };
+  if (password) updateData = { password };
+  // 2 update User
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+
+  // 3- return Data when there is no avatar this mean to return data when we don't make any update in avatar
+  if (!avatar) return data;
+
+  // 4- Upload avatar to avatars Bucket
+  const fileName = `avatar-${data?.user.id}`;
+  const { error: storageError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName);
+  if (storageError) throw new Error(storageError.message);
+
+  // 5- Update avatar in the user
+  const { data: updateUser, error: error2 } = await supabase.auth.updateUser({
+    data: {
+      avatar: `${SUPBASEURL}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+  return updateUser;
 }

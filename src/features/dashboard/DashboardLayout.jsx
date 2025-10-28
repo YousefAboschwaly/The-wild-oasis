e27b213988b4a09e34861c444b01ad77a03778db
yousef <yousefshwaly@gmail.com>
2025-10-29 00:33:02 +0300

    Create actual Data depend on bookings

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -19,13 +19,12 @@ export default function DashboardLayout() {
   const {stays,confirmedStays,isPending:isPending2 , numDays} = useRecentStays()
   const {cabins , isPending:isPending3} = useCabins()
   if(isPending || isPending2 || isPending3) return <Spinner/>
-
   return (
     <StyledDashboardLayout>
       <Stats bookings={bookings} confirmedStays={confirmedStays} numDays={numDays} numCabins={cabins.length} />
       <div>Today's and activity</div>
       <div>Charts and durations</div>
-      <SalesChart/>
+      <SalesChart numDays={numDays} bookings={bookings}/>
     </StyledDashboardLayout>
   )
 }

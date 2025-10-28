4049ccfc7c3aa409d6675a9c41d3be274305837d
yousef <yousefshwaly@gmail.com>
2025-10-28 23:53:46 +0300

    Change colors of Axises and Tooltip backgroundColor

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -54,31 +54,38 @@ const fakeData = [
   { label: "Feb 06", totalSales: 1450, extrasSales: 400 },
 ];
 
-
 export default function SalesChart() {
-  const {isDarkMode} = useDarkMode();
-const colors = isDarkMode
-  ? {
-      totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
-      extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
-      text: "#e5e7eb",
-      background: "#18212f",
-    }
-  : {
-      totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
-      extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
-      text: "#374151",
-      background: "#fff",
-    };
+  const { isDarkMode } = useDarkMode();
+  const colors = isDarkMode
+    ? {
+        totalSales: { stroke: "#4f46e5", fill: "#4f46e5" },
+        extrasSales: { stroke: "#22c55e", fill: "#22c55e" },
+        text: "#e5e7eb",
+        background: "#18212f",
+      }
+    : {
+        totalSales: { stroke: "#4f46e5", fill: "#c7d2fe" },
+        extrasSales: { stroke: "#16a34a", fill: "#dcfce7" },
+        text: "#374151",
+        background: "#fff",
+      };
 
   return (
     <StyledSalesChart>
       <Heading as="h2">Sales</Heading>
       <ResponsiveContainer width={"100%"} height={350}>
-        <AreaChart data={fakeData} >
-          <XAxis dataKey={"label"} />
-          <YAxis unit="$" />
-          <Tooltip />
+        <AreaChart data={fakeData}>
+          <XAxis
+            dataKey={"label"}
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <YAxis
+            unit="$"
+            tick={{ fill: colors.text }}
+            tickLine={{ stroke: colors.text }}
+          />
+          <Tooltip contentStyle={{backgroundColor:colors.background}} />
           <CartesianGrid strokeDasharray={4} />
           <Area
             dataKey={"totalSales"}
